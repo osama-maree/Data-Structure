942abd96c9ada4ecbb88fd675303b187566c5509
Osama Maree <108696087+osama-maree@users.noreply.github.com>
2022-09-21 11:52:22 +0300

    Node For Tree

--- Node.java
+++ Node.java
@@ -1,28 +1,14 @@
-package arraylist;
 
+package tree;
 public class Node<E> {
 
-    Node<E> next;
-    E element;
-
-    public Node(E e) {
-        this.element = e;
-    }
-
-    public Node<E> getNext() {
-        return next;
-    }
-
-    public void setNext(Node<E> next) {
-        this.next = next;
-    }
-
-    public E getElement() {
-        return element;
-    }
-
-    public void setElement(E element) {
-        this.element = element;
-    }
-
+	E Value;
+	Node<E> Left , Right;
+	
+	Node(E Value)
+	{
+		this.Value = Value;
+		this.Left = this.Right = null;
+	}
+	
 }
